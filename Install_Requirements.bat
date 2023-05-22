bed95d4ecb81ae87abe9e38f5bb3bff2f413926b
NanoSyncHole <134250659+NanoSyncHole@users.noreply.github.com>
2023-05-21 23:42:37 -0400

    Update and rename Install Requirements.bat to Install_Requirements.bat
    Add some ease of install.

---
+++ Install_Requirements.bat
@@ -0,0 +1,10 @@
+python -m pip install colorama==0.4.4
+python -m pip install PyAutoGUI==0.9.53
+python -m pip install keyboard==0.13.5
+
+:: Bat file appear to run with shell. if python was installed
+:: with an ide it seems to not understand pip as a command
+:: adding python -m to the front should not cause an issue
+:: and help if pip is only a python module or whatever
+:: changed name to make more command line friendly/
+:: tired of using ''  or  ""
